08d35522630dd490fe8fae1d0b665cee8135239e
Dustin Ray <dustinray@gmail.com>
2020-05-12 12:37:45 -0500

    added san mateo county and simplified the code added additional annotation

--- covid_cases.R
+++ covid_cases.R
@@ -3,13 +3,13 @@ library(tidyverse)
 
 covid <- read_csv(url("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"))
 
+
+######## St. Louis County ########
+
 #select st. louis county
 
-county <- covid %>% filter(state=="Minnesota", county=="St. Louis")
-
-county2 <- covid %>% filter(state=="Minnesota", county=="St. Louis")
-
-county2 <-  mutate(county,diff = cases - lag(cases, default = first(cases)))
+st.louis <- covid %>% filter(state=="Minnesota", county=="St. Louis") %>% 
+  mutate(county,diff = cases - lag(cases, default = first(cases)))
 
 
 covid %>% filter(state=="Minnesota", county=="St. Louis") %>% 
@@ -19,5 +19,18 @@ covid %>% filter(state=="Minnesota", county=="St. Louis") %>%
   theme_classic()
 
 
-ggplot(county2, aes(date, diff)) + geom_col() + theme_classic()
+ggplot(st.louis, aes(date, diff)) + geom_col() + theme_classic()
 
+
+
+######### San Mateo County ########
+san.mat <- covid %>% filter(state=="California", county=="San Mateo") %>% 
+          mutate(county,diff = cases - lag(cases, default = first(cases)))
+
+ggplot(san.mat, aes(date, cases, colour=county)) + 
+  geom_line() +
+  # geom_line(aes(date, deaths)) +
+  theme_classic()
+
+ggplot(san.mat, aes(date, diff)) + geom_col() + theme_classic() +
+  labs(title="Daily New Cases of Covid-19 - San Mateo County, CA", x= "Date", y="New Cases")
